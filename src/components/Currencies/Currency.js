d2fb07cd956cd2fc1c6e902496cc32f16b3a3564
vincentferniotoclock <vincent-ferniot@oclock.io>
2021-07-13 10:36:21 +0200

    JSX + style de Currencies

---
+++ src/components/Currencies/Currency.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Currency() {
+  return (
+    <li className="currency">United States Dollar</li>
+  );
+}
--- src/components/Currencies/index.js
+++ src/components/Currencies/index.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import Currency from './Currency';
 
 import './style.scss';
 
 export default function Currencies() {
   return (
-    <div>Currencies</div>
+    <div className="currencies">
+      <p className="currencies__title">Currencies</p>
+      <ul className="currencies__list">
+        <Currency />
+        <Currency />
+        <Currency />
+        <Currency />
+        <Currency />
+        <Currency />
+        <Currency />
+        <Currency />
+        <Currency />
+        <Currency />
+        <Currency />
+        <Currency />
+        <Currency />
+        <Currency />
+        <Currency />
+        <Currency />
+        <Currency />
+        <Currency />
+        <Currency />
+        <Currency />
+      </ul>
+    </div>
   );
 }
