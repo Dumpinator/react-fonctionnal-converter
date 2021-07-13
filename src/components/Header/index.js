aafa41a4501cc98a6886cddc7020638768379995
vincentferniotoclock <vincent-ferniot@oclock.io>
2021-07-13 10:16:47 +0200

    JSX + style de Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,6 +4,9 @@ import './style.scss';
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <header className="header">
+      <h1 className="header__title">Converter</h1>
+      <p className="header__base-amount">1 Euro</p>
+    </header>
   );
 }
