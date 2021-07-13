84e0075e7584f16dbbc9706e7177e03fc0177e8a
vincentferniotoclock <vincent-ferniot@oclock.io>
2021-07-13 09:34:59 +0200

    découpe statique

--- src/components/Converter/index.js
+++ src/components/Converter/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 export default function Converter() {
   return (
-    <div className="converter">Converter</div>
+    <div className="converter">
+      <header>Header</header>
+      <ul>
+        <li>Currencies</li>
+      </ul>
+      <footer>Amount</footer>
+    </div>
   );
 }
