283335e616c7f80017bf75ffe4e5292e8f548cc2
vincentferniotoclock <vincent-ferniot@oclock.io>
2021-07-13 10:21:44 +0200

    JSX + style de Amount

--- src/components/Amount/index.js
+++ src/components/Amount/index.js
@@ -4,6 +4,9 @@ import './style.scss';
 
 export default function Amount() {
   return (
-    <div>Amount</div>
+    <footer className="amount">
+      <p className="amount__value">1.09</p>
+      <p className="amount__currency">United States Dollar</p>
+    </footer>
   );
 }
